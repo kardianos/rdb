eef8dea9fbb1c98d07a161006a64559ee75a969f
Daniel Theophanes <kardianos@gmail.com>
2014-05-19 21:35:55 -0700

    rdb/pg: ignore old test.

--- pg/bench_test.go
+++ pg/bench_test.go
@@ -1,8 +1,8 @@
-// +build go1.1
-
 // Copyright (c) 2011, The pg Authors. All Rights Reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+
+// +build ignore
 
 package pg
 
