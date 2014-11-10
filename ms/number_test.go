cdba71534b87d3bf200d931fcb2afbd697b47f96
Daniel Theophanes <kardianos@gmail.com>
2014-11-09 18:19:37 -0800

    rdb/ms: add more tests.

--- ms/number_test.go
+++ ms/number_test.go
@@ -19,7 +19,7 @@ func TestNumber(t *testing.T) {
 		Sql: `
 			select
 				bt = @bt, bf = @bf,
-				i8 = @i8, i16 = @i16,
+				i8 = @i8, i16 = @i16, i64 = @i64,
 				bb = @bb,
 				dec = @dec,
 				fl32 = @fl32,
@@ -36,11 +36,14 @@ func TestNumber(t *testing.T) {
 	var fl32 float32
 	var fl64 float64
 
+	var i64 int64 = 1234567
+
 	params := []rdb.Param{
 		{Name: "bt", Type: rdb.TypeBool, Value: true},
 		{Name: "bf", Type: rdb.TypeBool, Value: false},
 		{Name: "i8", Type: rdb.TypeInt8, Value: byte(55)},
 		{Name: "i16", Type: rdb.TypeInt16, Value: int16(1234)},
+		{Name: "i64", Type: rdb.Integer, Value: i64},
 		{Name: "bb", Type: rdb.Binary, Length: 0, Value: []byte{23, 24, 25, 26, 27}},
 		{Name: "dec", Type: rdb.TypeDecimal, Precision: 38, Scale: 4, Value: big.NewRat(1234, 100)},
 		{Name: "fl32", Type: rdb.TypeFloat32, Value: float32(45.67)},
@@ -50,9 +53,42 @@ func TestNumber(t *testing.T) {
 	res := db.Query(cmd, params...)
 	defer res.Close()
 
-	res.Scan(&bt, &bf, &i8, &i16, &bb, &dec, &fl32, &fl64)
+	res.Scan(&bt, &bf, &i8, &i16, &i64, &bb, &dec, &fl32, &fl64)
 
-	compare := []interface{}{bt, bf, i8, i16, bb, dec, fl32, fl64}
+	compare := []interface{}{bt, bf, i8, i16, i64, bb, dec, fl32, fl64}
+
+	for i := range compare {
+		in := params[i]
+		if !reflect.DeepEqual(compare[i], in.Value) {
+			t.Errorf("Param %s did not round trip: Want (%v) got (%v)", in.Name, in.Value, compare[i])
+		}
+	}
+}
+
+func TestBytesValue(t *testing.T) {
+	defer recoverTest(t)
+
+	cmd := &rdb.Command{
+		Sql: `
+			select @bytesEmpty, @bytesOne
+		`,
+		Arity: rdb.OneMust,
+	}
+
+	bytesEmpty, bytesOne := []byte{}, []byte{01}
+	var bytesEmptyOut, bytesOneOut []byte
+
+	params := []rdb.Param{
+		{Name: "bytesEmpty", Type: rdb.Binary, Value: bytesEmpty},
+		{Name: "bytesOne", Type: rdb.Binary, Value: bytesOne},
+	}
+
+	res := db.Query(cmd, params...)
+	defer res.Close()
+
+	res.Scan(&bytesEmptyOut, &bytesOneOut)
+
+	compare := []interface{}{bytesEmptyOut, bytesOneOut}
 
 	for i := range compare {
 		in := params[i]
