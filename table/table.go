aa476a1bd262484bc0e6ca058ce0ed906a81e188
Daniel Theophanes <kardianos@gmail.com>
2020-11-02 13:19:20 -0600

    fix Buffer.AddBufferRow

--- table/table.go
+++ table/table.go
@@ -162,8 +162,7 @@ func (b *Buffer) AddRow(v ...interface{}) *Row {
 	return r
 }
 
-func (b *Buffer) AddBufferRow(row *Row) {
-	x := *row
-	x.buffer = b
+func (b *Buffer) AddBufferRow(row Row) {
+	row.buffer = b
 	b.Row = append(b.Row, x)
 }
